b88ae87b9f868ea49ae102db1024e53f94cd2096
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2022-11-09 20:02:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ function preload() {
 function setup() {
 canvas = createCanvas(500 , 500);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(500 , 500);
+poseNet = ml5.poseNet(video , modelLoaded);
+poseNet.on('pose' , gotPoses);
 
 }
 function draw() {
 
+}
+function modelLoaded () {
+    console.log("poseNet is Initialized");
+}
+
+function gotPoses(results) {
+if (results.length > 0) {
+    console.log(results);
+}
 }
